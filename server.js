a255f69f4ba43725f5a9372c704d797d5ea4a264
Luke Ford <Luke.ford@outlook.com>
2025-05-03 15:42:30 +1000

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import Fastify from 'fastify';
 import fastifyCors from '@fastify/cors';
 import fastifyFormBody from '@fastify/formbody';
 import fastifyWs from '@fastify/websocket';
-import { handleCallWebhook, handleMediaStreamSocket } from './twilioHandler.js';
 import dotenv from 'dotenv';
+import { handleCallWebhook, handleMediaStreamSocket } from './twilioHandler.js';
 
 dotenv.config();
 
 const app = Fastify({ logger: true });
 
-// register plugins
+// register Fastify plugins
 app.register(fastifyCors);
 app.register(fastifyFormBody);
 app.register(fastifyWs);
@@ -20,7 +20,6 @@ app.post('/start-call', handleCallWebhook);
 // WebSocket endpoint for Twilio Media Streams
 app.get('/media-stream', { websocket: true }, handleMediaStreamSocket);
 
-// start server
 const PORT = process.env.PORT || 3000;
 app.listen({ port: PORT, host: '0.0.0.0' })
    .then(() => app.log.info(`Listening on ${PORT}`));
